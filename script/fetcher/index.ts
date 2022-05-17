c8cba84d7301c991881e308a00a99f39103e46de
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-05-17 23:53:39 +0900

    undefinedの可能性を考慮

---
+++ script/fetcher/index.ts
@@ -0,0 +1,34 @@
+import { request, GraphQLClient } from 'graphql-request'
+import {
+	FetchKibelaArticleContentQueryDocument,
+	FetchPostNumberDocument,
+	getSdk
+} from '../graphql/types'
+
+const headers = {
+	Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+	'Content-Type': 'application/json'
+}
+
+const client = new GraphQLClient(`https://${process.env.KIBELA_DOMAIN}/api/v1`)
+const sdk = getSdk(client)
+
+export const fetchAllKibelaArticle = async () => {
+	try {
+		const postNumberResponse = await sdk.fetchPostNumber(
+			{ first: 1 },
+			headers
+		)
+
+		if (postNumberResponse.currentUser) {
+			const allArticleContent = await sdk.fetchKibelaArticleContentQuery(
+				{ first: postNumberResponse.currentUser.latestNotes.totalCount},
+				headers
+			)
+			return allArticleContent.currentUser ? allArticleContent.currentUser.latestNotes.nodes : []
+		}
+
+	} catch (e) {
+		console.log(e)
+	}
+}
