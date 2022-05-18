9d870b1c0df86844c81ec65105b83eeec12146a4
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-05-18 23:49:23 +0900

    markdownファイルを生成するスクリプトの作成

--- script/index.ts
+++ script/index.ts
@@ -1,30 +1,25 @@
 import 'dotenv/config'
 import { Command } from 'commander'
-import { request } from 'graphql-request'
-import query from './query/fetchKibelaArticle'
+import { fetchAllKibelaArticle } from './fetcher'
+import TurndownService = require('turndown')
+import * as fs from 'fs'
 
 const program = new Command()
 program.parse(process.argv)
 
-
 // TODO: KibelaAPIから最新の記事のHTMLと全ての投稿のHTMLを取得する処理を作成する
 // TODO; 取得したHTMLをパースしてMarkdown形式に変換できるようなライブラリを調査する
-const headers = {
-	Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
-	'Content-Type': 'application/json'
-}
 
-const fetchKibelaArticle = async () => {
-	try {
-		const res = await request({
-			url: `https://${process.env.KIBELA_DOMAIN}/api/v1`,
-			document: query,
-			requestHeaders: headers
+
+fetchAllKibelaArticle().then((response) => {
+	if (response) {
+		const turndownService = new TurndownService()
+		response.forEach(element => {
+			const markdown = turndownService.turndown(element.contentHtml)
+			fs.appendFile(`${__dirname}/${element.title}.md`, markdown, (err) => {
+				console.log(err)
+			})
 		})
-		console.log(res)
-	} catch (e) {
-		console.log(e)
+		console.log(__dirname)
 	}
-}
-
-fetchKibelaArticle()
+})
