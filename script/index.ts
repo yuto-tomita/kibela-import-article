aa16595879d7f82aa3d5b99348ddf2b740280024
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-05-22 15:53:54 +0900

    kibelaディレクトリを作成して、HTMLデータをMarkdown形式に変換する処理を記述

--- script/index.ts
+++ script/index.ts
@@ -1,25 +1,35 @@
 import 'dotenv/config'
 import { Command } from 'commander'
-import { fetchAllKibelaArticle } from './fetcher'
+import { fetchAllKibelaArticle, fetchFirstKibelaArticle } from './fetcher'
 import TurndownService = require('turndown')
+import * as readline from 'node:readline';
 import * as fs from 'fs'
 
 const program = new Command()
 program.parse(process.argv)
 
-// TODO: KibelaAPIから最新の記事のHTMLと全ての投稿のHTMLを取得する処理を作成する
-// TODO; 取得したHTMLをパースしてMarkdown形式に変換できるようなライブラリを調査する
-
+fs.mkdirSync(`${__dirname}/kibela`)
 
 fetchAllKibelaArticle().then((response) => {
 	if (response) {
 		const turndownService = new TurndownService()
 		response.forEach(element => {
 			const markdown = turndownService.turndown(element.contentHtml)
-			fs.appendFile(`${__dirname}/${element.title}.md`, markdown, (err) => {
+			fs.appendFile(`${__dirname}/kibela/${element.title}.md`, markdown, (err) => {
 				console.log(err)
 			})
 		})
-		console.log(__dirname)
+	}
+})
+
+fetchFirstKibelaArticle().then((response) => {
+	if (response) {
+		const turndownService = new TurndownService()
+		response.forEach(element => {
+			const markdown = turndownService.turndown(element.contentHtml)
+			fs.appendFile(`${__dirname}/kibela/${element.title}.md`, markdown, (err) => {
+				console.log(err)
+			})
+		})
 	}
 })
