4397539e4fbd644164a2113341f2ad300b9865f7
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-05-22 22:23:57 +0900

    対話型のcliの実装

--- script/index.ts
+++ script/index.ts
@@ -2,34 +2,71 @@ import 'dotenv/config'
 import { Command } from 'commander'
 import { fetchAllKibelaArticle, fetchFirstKibelaArticle } from './fetcher'
 import TurndownService = require('turndown')
-import * as readline from 'node:readline';
 import * as fs from 'fs'
-
-const program = new Command()
-program.parse(process.argv)
+import inquirer = require('inquirer')
 
 fs.mkdirSync(`${__dirname}/kibela`)
 
-fetchAllKibelaArticle().then((response) => {
-	if (response) {
-		const turndownService = new TurndownService()
-		response.forEach(element => {
-			const markdown = turndownService.turndown(element.contentHtml)
-			fs.appendFile(`${__dirname}/kibela/${element.title}.md`, markdown, (err) => {
-				console.log(err)
-			})
-		})
-	}
-})
+const outputFile = (response: {
+	__typename?: "Note";
+	title: string;
+	contentHtml: string;
+}[]) => {
+	const turndownService = new TurndownService()
 
-fetchFirstKibelaArticle().then((response) => {
-	if (response) {
-		const turndownService = new TurndownService()
-		response.forEach(element => {
-			const markdown = turndownService.turndown(element.contentHtml)
-			fs.appendFile(`${__dirname}/kibela/${element.title}.md`, markdown, (err) => {
-				console.log(err)
+	response.forEach((element) => {
+		const markdown = turndownService.turndown(element.contentHtml)
+		if (element.title.includes(' ') || element.title.includes('/')) {
+			// TODO: Node.js実行環境をv15~と記述するようにする
+	
+			const removeBlank = element.title.replaceAll(' ', '_')
+			const removeSlash = removeBlank.replaceAll('/', '_')
+			fs.appendFile(`${__dirname}/kibela/${removeSlash}.md`, markdown, (err) => {
+				if (err) console.log(err)
 			})
-		})
-	}
-})
+		} else {
+			fs.appendFile(`${__dirname}/kibela/${element.title}.md`, markdown, (err) => {
+				if (err) console.log(err)
+			})
+		}
+	})
+}
+
+inquirer
+	.prompt([
+		{
+			type: 'input',
+			name: 'accessToken',
+			message: 'Kibelaのアクセストークンを入力してください',
+			validate: (input) => !!input.length,
+		},
+		{
+			type: 'list',
+			name: 'script',
+			message: '実行するスクリプトを選択してください',
+			choices: [
+				'全てのKibela記事をローカルにインポート',
+				'最新Kibela記事をローカルにインポート'
+			]
+		}
+	])
+	.then(answers => {
+		console.log(answers)
+
+		if (answers.script === '全てのKibela記事をローカルにインポート') {
+			fetchAllKibelaArticle().then((response) => {
+				if (response) {
+					outputFile(response)
+				}
+			})
+		} else if (answers.script === '最新Kibela記事をローカルにインポート') {
+			fetchFirstKibelaArticle().then((response) => {
+				if (response) {
+					outputFile(response)
+				}
+			})
+		}
+	})
+
+const program = new Command()
+program.parse(process.argv)
